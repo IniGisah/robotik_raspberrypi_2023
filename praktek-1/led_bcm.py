e2d1dcf3d2c47a7922c4ff26e48360e0f456196b
Syarif Hilmi Ramadhani <40476986+syarifhilmi@users.noreply.github.com>
2021-08-25 05:20:38 +0700

    Update led_bcm.py
    fix while true

--- praktek-1/led_bcm.py
+++ praktek-1/led_bcm.py
@@ -4,13 +4,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT, initial=GPIO.LOW)
-while True :
- try:
+
+try:
+  while True :
     print ("LED on")
     GPIO.output(18,GPIO.HIGH)
     time.sleep(1)
     print ("LED off")
     GPIO.output(18,GPIO.LOW)
     time.sleep(1)
- finally :
+finally :
     GPIO.cleanup()
